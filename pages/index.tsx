43385aba767b982134d059c53339f9f4d6965e3c
John Xander <johnblox5@gmail.com>
2022-11-17 17:41:04 +0100

    add basic UI structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
 export default function Home() {
   return (
-    <div>Hello World!</div>
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which pokemon is rounder?</div>
+      <div className="p-2"></div>
+      <div className="border rounded p-8 flex items-center justify-between max-w-2xl">
+        <div className="w-16 h-16 bg-red-200"></div>
+          <div className="p-8">vs.</div>
+          <div className="w-16 h-16 bg-red-200"></div>
+      </div>
+    </div>
   )
 }
