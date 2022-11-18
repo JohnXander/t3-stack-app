028f8f21df37bad0439f2b8ab088a149fde17a6d
John Xander <johnblox5@gmail.com>
2022-11-18 21:06:27 +0100

    fix tailwind connection bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,17 @@
+import { getOptionsForVote } from '../utils/getRandomPokemon';
 import { trpc } from '../utils/trpc';
 
 export default function IndexPage() {
-  const {data, isLoading} = trpc.hello.useQuery({ text: 'John' });
-  
-  if (isLoading) return <div>Loading...</div>;
-
-  if (data) return <div>{data.greeting}</div>
+  const [first, second] = getOptionsForVote();
 
   return (
-    <div className='h-screen w-screen flex flex-col justify-content items-center'>
+    <div className='h-screen w-screen flex flex-col justify-center items-center text-white bg-gray-800'>
       <div className='text-2xl text-center'>Which pokemon is rounder?</div>
       <div className='p-2'></div>
       <div className='border rounded p-8 flex justify-between items-center max-w-2xl'>
-        <div className='w-16 h-16 bg-red-200'></div>
+        <div className='w-16 h-16 bg-red-800'>{first}</div>
         <div className='p-8'>vs.</div>
-        <div className='w-16 h-16 bg-red-200'></div>
+        <div className='w-16 h-16 bg-red-800'>{second}</div>
       </div>
     </div>
   );
