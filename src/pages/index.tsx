f781565f96c55cf93c38f15bd34159aee588a790
John Xander <johnblox5@gmail.com>
2022-11-20 15:31:03 +0100

    add pokemon names

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,25 +12,32 @@ export default function IndexPage() {
 
   if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
 
-  const firstPokeImg: string = String(firstPokemon.data?.sprites?.front_default);
-  const secondPokeImg: string = String(secondPokemon.data?.sprites?.front_default);
+  const firstPokeImg: string = String(firstPokemon.data?.sprites.front_default);
+  const secondPokeImg: string = String(secondPokemon.data?.sprites.front_default);
 
   return (
     <div className='h-screen w-screen flex flex-col justify-center items-center text-white bg-gray-800'>
       <div className='text-2xl text-center'>Which pokemon is rounder?</div>
       <div className='p-2'></div>
       <div className='border rounded p-8 flex justify-between items-center max-w-2xl'>
-        <div className='w-16 h-16 bg-red-800'>
+        <div className='w-64 h-64 flex flex-col'>
           <picture>
-            <img src={firstPokeImg} alt="" />
+            <img src={firstPokeImg} alt="" className='w-full' />
           </picture>
+          <div className='text-xl text-center capitalize mt-[-2rem]'>
+            {firstPokemon.data?.name}
+          </div>
         </div>
         <div className='p-8'>vs.</div>
-        <div className='w-16 h-16 bg-red-800'>
+        <div className='w-64 h-64 flex flex-col'>
           <picture>
-            <img src={secondPokeImg} alt="" />
+            <img src={secondPokeImg} alt="" className='w-full' />
           </picture>
+          <div className='text-xl text-center capitalize mt-[-2rem]'>
+            {secondPokemon.data?.name}
+          </div>
         </div>
+        <div className='p-2'></div>
       </div>
     </div>
   );
